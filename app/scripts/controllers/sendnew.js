c87a25bf6ec7b5f5bca3f4d37f6ac7ffbbd38f73
Gwak Seon-Geun <nicholas5154@naver.com>
2016-10-20 02:57:46 +0900

    Send-new with data from Daum Maps API

--- app/scripts/controllers/sendnew.js
+++ app/scripts/controllers/sendnew.js
@@ -9,80 +9,129 @@
 */
 angular.module('everyquickApp')
 .controller('SendNewCtrl', 
-	['$scope', '$firebaseArray',
-	function($scope, $firebaseArray) {
+	['$scope', '$firebaseArray', 'Auth', 
+	function($scope, $firebaseArray, Auth) {
 		$scope.formData = {
-			start_addr:"",
-			end_addr:"",
-			price:0,
+			dep:{},
+			dest:{},
+			depList:[],
+			destList:[],
+			price:3000
 		};
 
-		var container = document.getElementById('map');
+		var depMapContainer = document.getElementById('dep_map');
+		var destMapContainer = document.getElementById('dest_map');
 		var options = {
 			center: new daum.maps.LatLng(33.450701, 126.570667),
 			level: 3
 		};
 
-		var map, ps;
+		var depMap, destMap, places;
+		var depMarker, destMarker;
 
-		daum.maps.load(function() {
-			map = new daum.maps.Map(container, options);
-			ps = new daum.maps.services.Places();
+		daum.maps.load(function(){
+			places = new daum.maps.services.Places();
+			depMap = new daum.maps.Map(depMapContainer, options);
+			destMap = new daum.maps.Map(destMapContainer, options);
+
+			
+			depMarker = new daum.maps.Marker({
+				map: depMap
+			});
+			destMarker = new daum.maps.Marker({
+				map: destMap
+			});
 		});
 
-		$scope.onSearchChange = function(){
-			console.log("change");
-			ps.keywordSearch($scope.formData.start_addr, placesSearchCB); 
+		$scope.depNameChanged = function(){
+			if($scope.formData.dep.title.length == 0) return;
+			places.keywordSearch($scope.formData.dep.title, depResult)
+		};
+
+		$scope.destNameChanged = function(){
+			if($scope.formData.dest.title.length == 0) return;
+			places.keywordSearch($scope.formData.dest.title, destResult)
+		};
+
+		function depResult (status, data, pagination) {
+			if (status === daum.maps.services.Status.OK) {
+				$scope.formData.depList = data.places;
+			}
 		}
 
-
-		// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-		function placesSearchCB (status, data, pagination) {
-		    if (status === daum.maps.services.Status.OK) {
-
-		        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-		        // LatLngBounds 객체에 좌표를 추가합니다
-		        var bounds = new daum.maps.LatLngBounds();
-
-		        for (var i=0; i<data.places.length; i++) {
-		            displayMarker(data.places[i]);    
-		            bounds.extend(new daum.maps.LatLng(data.places[i].latitude, data.places[i].longitude));
-		        }       
-
-		        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-		        map.setBounds(bounds);
-		    } 
+		function destResult (status, data, pagination) {
+			if (status === daum.maps.services.Status.OK) {
+				$scope.formData.destList = data.places;
+			}
 		}
 
-		// 지도에 마커를 표시하는 함수입니다
-		function displayMarker(place) {
-		    
-		    // 마커를 생성하고 지도에 표시합니다
-		    var marker = new daum.maps.Marker({
-		        map: map,
-		        position: new daum.maps.LatLng(place.latitude, place.longitude) 
-		    });
+		$scope.selectDep = function(depObj){
+			$scope.formData.dep = depObj;
+			console.log(depObj);
+			$scope.formData.depList = [];
+			depMap.setCenter(
+				new daum.maps.LatLng(
+					depObj.latitude, 
+					depObj.longitude
+					)
+				);
+			displayMarker(depObj, depMarker, depMap);
+		};
 
-		    // 마커에 클릭이벤트를 등록합니다
-		    daum.maps.event.addListener(marker, 'click', function() {
-		        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-		        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.title + '</div>');
-		        infowindow.open(map, marker);
-		    });
+		$scope.selectDest = function(destObj){
+			$scope.formData.dest = destObj;
+			$scope.formData.destList = [];
+			destMap.setCenter(
+				new daum.maps.LatLng(
+					destObj.latitude, 
+					destObj.longitude
+					)
+				);
+			displayMarker(destObj, destMarker, destMap);
+		};
+
+		function displayMarker(place, marker, map) {
+			// 마커를 생성하고 지도에 표시합니다
+			marker.setPosition(
+				new daum.maps.LatLng(place.latitude, place.longitude)
+			);
 		}
+
+		$scope.onPriceSliderChange = function(){
+			$scope.formData.price = parseInt($scope.formData.price);
+		};
 
 		$scope.createDelivery = function(){
-		    var deliveriesRef = firebase.database().ref().child("deliveries");
-		    // download the data from a Firebase reference into a (pseudo read-only) array
-		    // all server changes are applied in realtime
-		    $scope.deliveries = $firebaseArray(deliveriesRef);
-		    // create a query for the most recent 25 deliveries on the server
-		    // the $firebaseArray service properly handles database queries as well
-		    $scope.deliveries.$add({
-		    	dep: $scope.formData.start_addr,
-		    	dest: $scope.formData.end_addr,
-		    	price: $scope.formData.price,
-		    });
-		}
-	}
-]);
+			var deliveriesRef = firebase.database().ref().child("deliveries");
+			// download the data from a Firebase reference into a (pseudo read-only) array
+			// all server changes are applied in realtime
+			$scope.deliveries = $firebaseArray(deliveriesRef);
+			// create a query for the most recent 25 deliveries on the server
+			// the $firebaseArray service properly handles database queries as well
+			$scope.deliveries.$add({
+				sender: Auth.$getAuth().uid,
+				dep: {
+					id: $scope.formData.dep.id,
+					title: $scope.formData.dep.title,
+					zipcode: $scope.formData.dep.zipcode,
+					address: $scope.formData.dep.address,
+					newAddress: $scope.formData.dep.newAddress,
+					latitude: $scope.formData.dep.latitude,
+					longitude: $scope.formData.dep.longitude
+				},
+				dest: {
+					id: $scope.formData.dest.id,
+					title: $scope.formData.dest.title,
+					zipcode: $scope.formData.dest.zipcode,
+					address: $scope.formData.dest.address,
+					newAddress: $scope.formData.dest.newAddress,
+					latitude: $scope.formData.dest.latitude,
+					longitude: $scope.formData.dest.longitude
+				},
+				price: $scope.formData.price,
+				posted: firebase.database.ServerValue.TIMESTAMP
+			});
+		};
+
+	}]
+);
