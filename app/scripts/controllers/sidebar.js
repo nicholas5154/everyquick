ba7bce158dc29aed8399e20a30bc33303cdcc006
Near-Nate <souseki21@naver.com>
2016-12-22 02:01:31 +0900

    Added filter on deliveries
    Added filter(by date, price, distance)

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -8,8 +8,8 @@
 * Controller of the everyquickApp
 */
 angular.module('everyquickApp')
-.controller('SidebarCtrl', ['$scope', '$ionicSideMenuDelegate',
-  function ($scope, $ionicSideMenuDelegate) {
+.controller('SidebarCtrl', ['$scope', '$ionicSideMenuDelegate', 'Auth',
+  function ($scope, $ionicSideMenuDelegate, Auth) {
     $ionicSideMenuDelegate.edgeDragThreshold(1)
     $ionicSideMenuDelegate.canDragContent(false)
   }
